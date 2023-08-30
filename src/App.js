df8153fb52e993b0d4f660ee72573f55812d62d1
UjjwalPoudel2003 <upoudel1@my.centennialcollege.ca>
2023-08-29 22:08:49 -0400

    Navbar Component Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
-
 export default App;
