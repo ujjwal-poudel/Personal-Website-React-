9d1da15e8c8d1d2ee997e3a21a31d16295ebc2bb
UjjwalPoudel2003 <upoudel1@my.centennialcollege.ca>
2023-08-29 22:06:50 -0400

    Created new NavItems list

---
+++ src/components/Navbar/NavItems.js
@@ -0,0 +1,33 @@
+// Creating a list of menu items for the navbar
+export const NavItems = [
+    {
+        title: 'Home',
+        url: '#',
+        urlClass: 'item',
+        cName: 'navbar-item'
+    },
+    {
+        title: 'About',
+        url: '#',
+        urlClass: 'item',
+        cName: 'navbar-item'
+    },
+    {
+        title: 'Skills',
+        url: '#',
+        urlClass: 'item',
+        cName: 'navbar-item'
+    },
+    {
+        title: 'Projects',
+        url: '#',
+        urlClass: 'item',
+        cName: 'navbar-item'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        urlClass: 'item',
+        cName: 'navbar-item'
+    }
+];
