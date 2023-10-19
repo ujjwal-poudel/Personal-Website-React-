8ea1eb75a448d59784370ce128e4477be8f6140f
UjjwalPoudel2003 <upoudel1@my.centennialcollege.ca>
2023-10-19 11:28:26 -0400

    Created items for navbar

---
+++ src/components/NavigationBar/Navitems.js
@@ -0,0 +1,10 @@
+const navItems = [
+    {name: 'Home', path: '/'},
+    {name: 'About', path: '/about'},
+    {name: 'Skills', path: '/skills'},
+    {name: 'Projects', path: '/projects'},
+    {name: 'Contact', path: '/contact'}
+
+];
+
+export default navItems;
