b0d6ca1f7a770f9b34636d4ef1f599d1d0604c44
UjjwalPoudel2003 <upoudel1@my.centennialcollege.ca>
2023-10-19 21:24:53 -0400

    responsive navbar set

--- src/components/NavigationBar/Navigation.js
+++ src/components/NavigationBar/Navigation.js
@@ -6,7 +6,10 @@ export default function Navigation() {
     return (
         <nav className='flex justify-between'>
             <h1><strong><Link to={'/'}>Ujjwal</Link></strong></h1>
-            <ul className='flex'>
+            <div className='block sm:hidden'>
+                <h3>Nab</h3>
+            </div>
+            <ul className='hidden sm:flex'>
                 {navItems.map(item => {
                     return (
                         <li key={item.name} className='ps-10'>
